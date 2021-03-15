c9d415df5bbf971a9fa536b87459af1eb7736be2
meaghan jennings <meaghanljennings@gmail.com>
2021-03-15 20:53:59 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,18 @@ require 'pry'
 class String
 
   def sentence?
+<<<<<<< HEAD
     if self.end_with?(".")
       true
     else
       false 
     end 
+=======
+    if String.end_with?(".")
+      true
+    else
+      false 
+>>>>>>> bf4149ab8135021266f4e4ccc5de7040c6d3b6a7
   end
 
   def question?
